6122e56c24f1d783b8319a9001127789c86fb1da
Johantheking17 <187cabot@gmail.com>
2024-12-07 02:40:37 +0000

    commit -m “submission 12/6”

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -2,12 +2,92 @@ import java.awt.*;
 
 public class Ball {
 	//your code here!
-	
-	//don't forget you need instance variables:
-	
-	
-	//constructor(s):
-	
-	//methods:
+//
+//  Class author:  John Taylor
+//  Date created:  12/5/24
+//  General description: This creates a ball object that has various propeties like position, size, and speed
+//
 
+
+
+
+	//don't forget you need instance variables:
+	private int xpos;
+	private int ypos;
+	private int size;
+	private int xVelocity;
+	private int yVelocity;
+	
+
+	//constructor(s):
+
+	Ball(int xpos, int ypos, int size){
+		this.xpos = xpos;
+		this.ypos = ypos;
+		this.size = size;
+		xVelocity = 1;
+		yVelocity = 1;
+	}
+	//methods:
+	public int getXpos(){
+		return xpos;
+	}
+
+	public int getYpos(){
+		return ypos;
+	}
+
+	public int getSize(){
+		return size;
+	}
+
+	public void setX(int newX){
+		xpos = newX;
+	}
+
+	public void setY(int newY){
+		ypos = newY;
+	}
+
+	public void setXVelocity(int newXVelocity){
+		xVelocity = newXVelocity;
+	}
+
+	public void setYVelocity(int newYVelocity){
+		yVelocity = newYVelocity;
+	}
+
+	//
+//  Pre-condition: nothing
+//  Post-condition: x velocity is reversed
+//
+	public void reverseX(){
+		xVelocity *= -1;
+	}
+
+	//
+//  Pre-condition: nothing
+//  Post-condition: y velocity is reversed
+//
+	public void reverseY(){
+		yVelocity *= -1;
+	}
+
+	//
+//  Pre-condition: nothing
+//  Post-condition: ball moves an increment according to the given velocities
+//
+	public void move(){
+		xpos += xVelocity;
+		ypos += yVelocity;
+	}
+//
+//  Pre-condition: g is an instance of Graphics
+//  Post-condition: this creates the ball on the screen
+//
+
+	public void draw(Graphics g){
+		g.setColor(Color.red);
+		g.fillOval(xpos, ypos, size, size);
+	}
 }
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,16 +1,86 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
 
 	//your code here!
+//
+//  Class author:  John Taylor
+//  Date created:  12/5/24
+//  General description: This create the paddle the user controls, moving side to side.
+//
+
+
 	
 	//don't forget you need instance variables:
-	
+	private int xpos;
+	private int ypos;
+	private int width;
+	private int height;
+	private int velocity;
 	
 	//constructor(s):
-	
+	Paddle(int xpos, int ypos, int width, int height){
+		this.xpos = xpos;
+		this.ypos = ypos;
+		this.width = width;
+		this.height = height;
+		velocity = 0;
+	}
 	//methods:
+	public int getX(){
+		return xpos;
+	}
+
+	public int getY(){
+		return ypos;
+	}
+
+	public int getWidth(){
+		return width;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
+	public int getVelocity(){
+		return velocity;
+	}
+
+	public void setX(int newX){
+		xpos = newX;
+	}
+
+	public void setVelocity(int newVelocity){
+		velocity = newVelocity;
+	}
+
+	//
+//  Pre-condition: the extravelocity is an integer
+//  Post-condition: the velocity increases given whats inn the argument
+//
+	public void addVelocity(int extraVelocity){
+		velocity += extraVelocity;
+	}
+
+	//
+//  Pre-condition: nothing
+//  Post-condition: the paddle moves an increment to the side according to the given velocity
+//
+	public void move(){
+		xpos += velocity;
+	}
+
+	//
+//  Pre-condition: g is an instance of Graphics
+//  Post-condition: the paddle si created on the screen
+//
+	public void draw(Graphics g){
+		g.setColor(Color.white);
+		g.fillRect(xpos, ypos, width, height);
+	}
+	
+	
 	
 	
 }
